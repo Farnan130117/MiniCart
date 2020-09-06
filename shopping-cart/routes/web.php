12377d10e37697bd6f5c15976350f6fddbe320c0
Farnan130117 <mh.farnan@gmail.com>
2020-09-06 15:46:31 +0600

    Adding Cart Controller and COnfigure routes\web.php file for routing.

--- shopping-cart/routes/web.php
+++ shopping-cart/routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+
+*/
+
+
+Route::get('/', 'CartController@shop')->name('shop');
+Route::get('/cart', 'CartController@cart')->name('cart.index');
+Route::post('/add', 'CartController@add')->name('cart.store');
+Route::post('/update', 'CartController@update')->name('cart.update');
+Route::post('/remove', 'CartController@remove')->name('cart.remove');
+Route::post('/clear', 'CartController@clear')->name('cart.clear');
